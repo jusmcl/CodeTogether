f3eaad66f603dd10e85d3be3fd6a0151bb19ea79
Armend Veseli <armendveseli@users.noreply.github.com>
2020-01-26 19:05:06 -0500

    Implemented GameContainer rendering methods.

--- src/dev/madtechservices/chess/ChessMain.java
+++ src/dev/madtechservices/chess/ChessMain.java
@@ -4,9 +4,10 @@ import dev.madtechservices.chess.engine.GameContainer;
 
 public class ChessMain {
 
-    GameContainer gameContainer = new GameContainer();
+    private static GameContainer gameContainer;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+         gameContainer = new GameContainer();
+         gameContainer.start();
     }
 }
--- src/dev/madtechservices/chess/engine/GameContainer.java
+++ src/dev/madtechservices/chess/engine/GameContainer.java
@@ -6,7 +6,7 @@ public class GameContainer implements Runnable{
 
     private boolean running = false;
 
-    private final double FRAME_CAP = 1.0 / 120;
+    private final double FRAME_CAP = 1.0 / 60;
 
     public GameContainer() {
 
@@ -26,19 +26,51 @@ public class GameContainer implements Runnable{
     }
 
     public void run() {
-
         running = true;
-
+        boolean render = false;
         double firstTime = 0, lastTime = 0, passedTime = 0, unProcessedTime = 0;
+        lastTime = System.nanoTime() / 1000000000.0;
+
+        double frameTime = 0;
+        int frames = 0;
+        int fps = 0;
 
         while (running) {
+            render = false;
+            firstTime = System.nanoTime() / 1000000000.0;
+            passedTime = firstTime - lastTime;
+            lastTime = firstTime;
+            unProcessedTime += passedTime;
+            frameTime += passedTime;
 
+            while(unProcessedTime >= FRAME_CAP){
+                unProcessedTime -= FRAME_CAP;
+                render = true;
+                //TODO: Update game
+
+                if(frameTime >= 1.0){
+                    frameTime = 0;
+                    fps = frames;
+                    frames = 0;
+                    System.out.println("FPS: " + fps);
+                }
+            }
+
+            if(render){
+                //TODO: render game
+                ++frames;
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+        dispose();
     }
 
     public void dispose() {
 
     }
-
-
 }
